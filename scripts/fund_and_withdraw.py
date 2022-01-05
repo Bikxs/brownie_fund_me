80343f976533fb8b2c20789a5f582fac4eb20d62
Stephen Biko Nyamai <biko.nyamai@gmail.com>
2022-01-05 12:17:08 +0300

    implement funding and withdrawing

--- scripts/fund_and_withdraw.py
+++ scripts/fund_and_withdraw.py
@@ -9,14 +9,23 @@ def fund():
         deploy_fund_me()
     fund_me = FundMe[-1]
     account = get_account()
-    entrance_fee = fund_me.getEntranceFee()
+    entrance_fee = fund_me.getEntranceFee()+100
     print(f"entrance_fee: {entrance_fee}")
+    print("Funding the account...")
+    fund_me.fund({"from": account, "value": entrance_fee})
+    print(f"Funds added: {entrance_fee}")
 
 
-def withdrawy():
+def withdraw():
+    if not FundMe:
+        deploy_fund_me()
+    fund_me = FundMe[-1]
     account = get_account()
-    FundMe.deploy({"from": account})
+    print("With drawing account...")
+    fund_me.withdraw({"from": account})
+    print(f"Funds withdrawn")
 
 
 def main():
     fund()
+    withdraw()
