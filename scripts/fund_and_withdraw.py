7c67f2d3f64a3d2866549d6cd4fae55717005ff4
Stephen Biko Nyamai <biko.nyamai@gmail.com>
2022-01-05 12:04:48 +0300

    deploy FundMe if not already deployed

--- scripts/fund_and_withdraw.py
+++ scripts/fund_and_withdraw.py
@@ -1,9 +1,12 @@
 from brownie import FundMe
 
+from scripts.deploy import deploy_fund_me
 from scripts.helpful_scripts import get_account
 
 
 def fund():
+    if not FundMe:
+        deploy_fund_me()
     fund_me = FundMe[-1]
     account = get_account()
 
