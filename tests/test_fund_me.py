36f619bffad9cd6ecd2fbfec4bb83ecfcc2437a1
Stephen Biko Nyamai <biko.nyamai@gmail.com>
2022-01-05 12:57:27 +0300

    test only owner can withdraw

--- tests/test_fund_me.py
+++ tests/test_fund_me.py
@@ -1,20 +1,37 @@
+import pytest
+from brownie import network, accounts, exceptions
+
 from scripts.deploy import deploy_fund_me
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 
 
 def test_can_deploy_fund_and_withdraw():
     account = get_account()
     # deploy
     fund_me = deploy_fund_me()
-    assert fund_me # assert that is object
+    assert fund_me  # assert that is object
 
     # funding
     entrance_fee = fund_me.getEntranceFee() + 100
     tx_fund = fund_me.fund({"from": account, "value": entrance_fee})
     tx_fund.wait(1)
-    assert fund_me.addressToAmountFunded(account.address) == entrance_fee #check that the funds have been added
+    assert fund_me.addressToAmountFunded(account.address) == entrance_fee  # check that the funds have been added
 
     # withdrawing
     tx_withdraw = fund_me.withdraw({"from": account})
     tx_withdraw.wait(1)
     assert fund_me.addressToAmountFunded(account.address) == 0  # that all funds haev been withdrawn
+
+
+def test_only_owner_can_withdraw():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local environment testing")
+    fund_me = deploy_fund_me()  # deployed from: account = get_account()
+    assert fund_me  # assert that is object
+
+    # attemp withdrawal
+    bad_actor = accounts.add()
+    # expect an exception
+
+    with pytest.raises(ValueError):
+        fund_me.withdraw({"from": bad_actor})
